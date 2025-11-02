22b1a3ab757260d8c25cac611c998e62e167928b
sanskar <sanskar.003.02@gmail.com>
2025-11-02 12:05:22 +0530

    ui enhancement

--- src/Pages/Page1.jsx
+++ src/Pages/Page1.jsx
@@ -3,48 +3,45 @@ import { useGSAP } from '@gsap/react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 
-const Page1 = () => {
-
-
 gsap.registerPlugin(ScrollTrigger);
 
-
-    useGSAP(function(){
-    gsap.to('.first',{
+const Page1 = () => {
+  useGSAP(() => {
+    gsap.to('.first', {
       rotation: '+=-15',
       translateX: '-=5%',
       transformOrigin: 'bottom left',
-      ease:'linear',
-      
-
+      ease: 'linear',
       scrollTrigger: {
         trigger: '.first',
         start: 'top top',
         end: 'bottom 25%',
         scrub: true,
-      }
-    })
-  })
+      },
+    });
+  });
 
-
-    return ( 
-        <>
-        <div className='relative overflow-hidden'>
-
-            <div className='first w-screen h-screen bg-[#ff0202] mix-blend-overlay z-10 flex'>
-              <div className='contrast-200 w-full h-full flex flex-col justify-end items-end p-4'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 126 22" class="header-logo__svg">
-                  <path d="M126 14.14V7.86h-8.397V6.28H126V0h-15.128v7.86h-1.689V0h-6.708v7.86h-1.689L95.744 0h-6.709v7.86h-1.688V0h-6.709v7.86h-8.397V0H65.51v7.86H45.362V6.28h8.42V0H38.653v7.86h-1.711V0h-6.708v7.86H25.19V0h-6.709v7.86h-6.73V6.28h5.042V0H0v6.28h5.043v1.58H0v6.28h5.043V22h6.708v-7.86h6.731V22h6.709v-7.86h5.043V22h6.708v-7.86h1.711V22h15.129v-6.28h-8.42v-1.58H65.51V22h13.44v-6.28h-6.71v-1.58h8.397V22h6.709v-7.86h1.688V22h6.709v-7.86h1.688l5.043 7.86h6.708v-7.86h1.689V22H126v-6.28h-8.397v-1.58z"></path>
-                </svg>
-              </div>
-            </div>
-
-            <div className='first-2 w-screen h-screen bg-transparent'>
-            </div>  
-
+  return (
+    <div className="relative overflow-hidden">
+      {/* Section 1 */}
+      <div className="first w-full h-screen bg-[#ff0202] mix-blend-overlay z-10 flex">
+        <div className="contrast-200 w-full h-full flex flex-col justify-end items-end p-4 sm:p-8 md:p-12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            viewBox="0 0 126 22"
+            className="w-40 sm:w-48 md:w-56"
+            aria-label="Logo"
+          >
+            <path d="M126 14.14V7.86h-8.397V6.28H126V0h-15.128v7.86h-1.689V0h-6.708v7.86h-1.689L95.744 0h-6.709v7.86h-1.688V0h-6.709v7.86h-8.397V0H65.51v7.86H45.362V6.28h8.42V0H38.653v7.86h-1.711V0h-6.708v7.86H25.19V0h-6.709v7.86h-6.73V6.28h5.042V0H0v6.28h5.043v1.58H0v6.28h5.043V22h6.708v-7.86h6.731V22h6.709v-7.86h5.043V22h6.708v-7.86h1.711V22h15.129v-6.28h-8.42v-1.58H65.51V22h13.44v-6.28h-6.71v-1.58h8.397V22h6.709v-7.86h1.688V22h6.709v-7.86h1.688l5.043 7.86h6.708v-7.86h1.689V22H126v-6.28h-8.397v-1.58z" />
+          </svg>
         </div>
-        </>
-     );
-}
- 
+      </div>
+
+      {/* Section 2 */}
+      <div className="first-2 w-full h-screen bg-transparent" />
+    </div>
+  );
+};
+
 export default Page1;
--- src/Pages/Page2.jsx
+++ src/Pages/Page2.jsx
@@ -1,260 +1,317 @@
-import React from 'react';
-import { useGSAP } from '@gsap/react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
-
-const Page2 = () => {
-
+import React from "react";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger);
 
+const Page2 = () => {
+  useGSAP(() => {
+    ScrollTrigger.matchMedia({
+      "(min-width: 1024px)": () => {
+        gsap.to(".second", {
+          rotation: -10,
+          xPercent: -10,
+          transformOrigin: "bottom left",
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".second",
+            start: "top 50%",
+            end: "bottom 70%",
+            scrub: true,
+          },
+        });
 
-  function pageEffect(){  
-  useGSAP(function(){
-    gsap.to('.second', {
-      rotation: '-=10',
-      translateX: '-=10%',
-      transformOrigin: 'bottom left',
-      ease:'linear',
-      
+        gsap.from(".third", {
+          rotation: 15,
+          yPercent: -60,
+          transformOrigin: "bottom left",
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".third",
+            start: "top 50%",
+            end: "bottom 70%",
+            scrub: true,
+          },
+        });
 
-      scrollTrigger: {
-        trigger: '.second',
-        start: 'top 50%',
-        end: 'bottom 70%',
-        scrub: true,
-        
-      }
-    })
-  })
+        gsap.from(".fourth", {
+          rotation: 16,
+          yPercent: -60,
+          transformOrigin: "bottom left",
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".fourth",
+            start: "top 70%",
+            end: "bottom 70%",
+            scrub: true,
+          },
+        });
 
-  useGSAP(function(){
-    gsap.from('.third', {
-      rotation: '+=15',
-      translateY: '-=60%',
-      transformOrigin: 'bottom left',
-      ease:'linear',
+        gsap.from(".fifth", {
+          rotation: 15,
+          yPercent: -60,
+          transformOrigin: "bottom left",
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".fifth",
+            start: "top 50%",
+            end: "bottom 70%",
+            scrub: true,
+          },
+        });
 
+        gsap.from(".sixth", {
+          rotation: 20,
+          xPercent: -50,
+          yPercent: -100,
+          transformOrigin: "bottom left",
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".sixth",
+            start: "top 110%",
+            end: "bottom 80%",
+            scrub: true,
+          },
+        });
+      },
 
-      scrollTrigger: {
-        trigger: '.third',
-        start: 'top 50%',
-        end: 'bottom 70%',
-        scrub: true,
-      }
-    })
-  })
-  
+      "(max-width: 1023px)": () => {
+        gsap.to(".second", {
+          rotation: -5,
+          xPercent: -5,
+          transformOrigin: "bottom left",
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".second",
+            start: "top 60%",
+            end: "bottom 80%",
+            scrub: true,
+          },
+        });
 
-  useGSAP(function(){
-    gsap.from('.fourth', {
-      rotation: '+=16',
-      translateY: '-=60%',
-      transformOrigin: 'bottom left',
-      ease:'linear',
+        gsap.from(".third", {
+          rotation: 8,
+          yPercent: -30,
+          transformOrigin: "bottom left",
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".third",
+            start: "top 60%",
+            end: "bottom 80%",
+            scrub: true,
+          },
+        });
 
+        gsap.from(".fourth", {
+          rotation: 10,
+          yPercent: -30,
+          transformOrigin: "bottom left",
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".fourth",
+            start: "top 70%",
+            end: "bottom 70%",
+            scrub: true,
+          },
+        });
 
-      scrollTrigger: {
-        trigger: '.fourth',
-        start: 'top 70%',
-        end: 'bottom 70%',
-        scrub: true,
-      }
-    })
-  })
-  
+        gsap.from(".fifth", {
+          rotation: 10,
+          yPercent: -30,
+          transformOrigin: "bottom left",
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".fifth",
+            start: "top 60%",
+            end: "bottom 80%",
+            scrub: true,
+          },
+        });
 
-  useGSAP(function(){
-    gsap.from('.fifth', {
-      rotation: '+=15',
-      translateY: '-=60%',
-      transformOrigin: 'bottom left',
-      ease:'linear',
+        gsap.from(".sixth", {
+          rotation: 10,
+          xPercent: -30,
+          yPercent: -50,
+          transformOrigin: "bottom left",
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".sixth",
+            start: "top 120%",
+            end: "bottom 90%",
+            scrub: true,
+          },
+        });
+      },
+    });
+  }, []);
 
-
-      scrollTrigger: {
-        trigger: '.fifth',
-        start: 'top 50%',
-        end: 'bottom 70%',
-        scrub: true,
-      }
-    })
-  })
-
-  useGSAP(function(){
-    gsap.from('.sixth', {
-      rotation: '+=20',
-      translateX: '-=50%',
-      translateY: '-=100%',
-      transformOrigin: 'bottom left',
-      ease:'linear',
-
-
-      scrollTrigger: {
-        trigger: '.sixth',
-        start: 'top 110%',
-        end: 'bottom 80%',
-        scrub: true,
-      }
-    })
-  })  
-
-}
-
-pageEffect();
-
-
-
-    return (
-
-        <>
-        
-        <div className='Page2 relative bg-[rgba(255,0,0,0.7)] backdrop-blur '>
-
-
-            <div className='second h-[115vh] w-screen bg-stone-100'>
-              <div className=' flex flex-col'>
-                <h4 className='text-[0.8em] p-[1em]'>
-                  / ABOUT THE LINE
-                </h4>
-              <h1 className='text-[10em] font-medium leading-[15vh]'>
-                Highlights
-              </h1>
-              <div className='flex gap-10 p-2'>
-              <h3 className='text-[2em] leading-[25vh] underline underline-offset-8 underline decoration-1'>
-                The Work 
-              </h3>
-              <h3 className='text-[2em] leading-[25vh] underline underline-offset-8 underline decoration-1 '>
-                The Studio
-              </h3>
-              </div>
-              </div>
-            </div>
-
-
-            <div className='third h-[130vh] w-screen bg-gray-300 p-4 sticky top-0 overflow-hidden '>
-              <div className='flex justify-between items-center'>
-                <h1 className='text-[5em] font-medium'>Azuki Elementals</h1>
-                <h2 className='text-[2em]'>View Project</h2>
-              </div>
-              <div className='h-full w-full border-2 border-dotted border-black p-4 '>
-                <div className='text-[0.7em] font-medium flex justify-between p-1 pr-2 pl-2 align-text-top'>
-                <h1>OVERSCAN</h1>
-                <h1>1577 x 974</h1>
-                </div>
-                <div className='h-full w-full border-solid border-4 border-black'>
-                    <video autoPlay loop muted className='w-full' src="src/assects/pageEffect-vid-01.mp4"></video>
-                </div>   
-              </div> 
-            </div>
-
-            <div className='h-[60vh] w-screen relative'></div>
-
-            <div className='fourth h-screen w-screen bg-gray-300 p-4 sticky top-0 '>
-              <div className='flex justify-between items-center'>
-                <h1 className='text-[5em] font-medium'>Delivery At Dawn</h1>
-                <h2 className='text-[2em]'>View Project</h2>
-              </div>
-              <div className='h-full w-full border-2 border-dotted border-black p-4 '>
-                <div className='text-[0.7em] font-medium flex justify-between p-1 pr-2 pl-2 align-text-top'>
-                <h1>OVERSCAN</h1>
-                <h1>1577 x 974</h1>
-                </div>
-                <div className='h-full w-full border-4 border-black'>
-                    <video autoPlay loop muted className='w-full' src="src/assects/pageEffect-vid-02.mp4"></video>
-                </div>   
-              </div>
-            </div>
-
-            <div className='h-[60vh] w-screen relative'></div>
-
-            <div className='fifth h-screen w-screen bg-gray-300 sticky top-0 p-4'>
-              <div className='flex justify-between items-center'>
-                <h1 className='text-[5em] font-medium'>Izzo</h1>
-                <h2 className='text-[2em]'>View Project</h2>
-              </div>
-              <div className='h-full w-full border-2 border-dotted border-black p-4 '>
-                <div className='text-[0.7em] font-medium flex justify-between p-1 pr-2 pl-2 align-text-top'>
-                <h1>OVERSCAN</h1>
-                <h1>1577 x 974</h1>
-                </div>
-                <div className='h-full w-full border-4 border-black'>
-                    <video autoPlay loop muted className='w-full' src="src/assects/pageEffext-vid-03.mp4"></video>
-                </div>   
-              </div>
-            </div>
-
-            <div className='h-[100vh] w-screen relative'></div>
-
-            <div className='sixth h-[160vh] w-screen bg-stone-100 sticky top-0 '>
-              <div className='p-2'>
-                <h2 className='text-[0.8em] pr-2 '>/ MAKE YOUR MARK</h2>
-                <h1 className='text-[11em] leading-[18vh] font-semibold'>The Studio</h1>
-
-                <div className='h-[30vh] w-full'></div>
-                
-                <div className='flex justify-between h-auto w-screen bg-stone-100 p-4'>
-
-                  <div className='secaction-left h-auto w-full relative p-10'>
-                    
-                      <img className='img1 w-[30vw] h-[90vh] ml-[13vw] rotate-[5deg]' src="src/assects/images/Seaction-img-two.avif" alt="" />
-                      <img className='img2 absolute w-[30vw] h-[90vh] top-24' src="src/assects/images/Seacion-img-one.avif" alt="" />
-                   
-                  </div>
-
-                  <div className='seaction-right h-full w-full p-10'>
-                    <h2 className='text-[0.8em] leading-[4vh] pr-2'>/ THE LOWDOWN</h2>
-                    <h1 className='leading-[3.5vh] text-[1.5em] '>
-                    We’re a globally renowned animation studio based in East LDN. We work across advertising interactive media, music videos and our own original content.
-                    </h1>
-                    <h2 className='text-[0.8em] pr-2 mt-16 leading-[5vh]'>/ AWARDS & MENTIONS</h2>
-                    <div className='Awards flex justify-between  text-[0.9em] font-normal leading-[2.5vh]'>
-                      <div className='awardUn underline underline-offset-4 decoration-1 '>
-                        <p>BAFTA</p>
-                        <p>AD YOUNG GUN AWARDS</p>
-                        <p>MEOW ANIMATION AWARDS</p>
-                        <p>CLIO</p>
-                        <p>MEOW ARROWS</p>
-                        <p>MEOW MUSIC VIDEO AWARDS</p>
-                        <p>D&AD</p>
-                      </div>
-                      <div className=''>
-                        <p>GQ</p>
-                        <p>NME</p>
-                        <p>THE VERGE</p>
-                        <p>VARIETY</p>
-                        <p>THE HOLLYWOOD REpOTER</p>
-                        <p>THE LOS ANGELES TIME</p>
-                        <p>THE TONIGHT SHOW WITH JIMMY FALLON</p>
-                        <p>BBC</p>
-                        <p>WIRED MAGAZINE</p>
-                        <p>THE INDEPENDENT</p>
-                        <p>THE SUNDAY TIME</p>
-                        <p>ROLLING STONE</p>
-                        <p>THE TELEGRApH</p>
-                        <p>pLAYBOY</p>
-                        <p>COSOMPOLITANT</p>
-                        <p>E ONLINE</p>
-                        <p>NERDIST</p>
-                        <p>NBC NEWS</p>
-                        <p>POLYGON</p>
-                        <p>HYPERBEAST</p>
-                      </div>
-                    </div>
-                    <div className='mt-10 '>
-                      <h1 className='text-[2em] font-large leading-[4vh]'>“BELLISSIMO”</h1>
-                      <h2 className='text-[0.8em] font-normal'>– GUILLERMO DEL TORO</h2>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
+  return (
+    <div className="Page2 relative bg-[rgba(255,0,0,0.7)] backdrop-blur">
+      {/* Section: Second */}
+      <section className="second min-h-screen w-full bg-stone-100 p-4">
+        <h4 className="text-sm">/ ABOUT THE LINE</h4>
+        <h1 className="text-[10vw] font-medium leading-[10vh]">Highlights</h1>
+        <div className="flex gap-6">
+          <h3 className="text-[5vw] underline underline-offset-4">The Work</h3>
+          <h3 className="text-[5vw] underline underline-offset-4">
+            The Studio
+          </h3>
         </div>
-        
-        </>
-     );
-}
- 
-export default Page2;
+      </section>
+
+      {/* Section: Third */}
+      <section className="third min-h-screen w-full bg-gray-300 p-4 sticky top-0 overflow-hidden">
+        <div className="flex justify-between items-center">
+          <h1 className="text-[8vw] font-medium">Azuki Elementals</h1>
+          <h2 className="text-[4vw]">View Project</h2>
+        </div>
+        <div className="border-2 border-dotted border-black p-4">
+          <div className="text-xs flex justify-between p-1">
+            <span>OVERSCAN</span>
+            <span>1577 x 974</span>
+          </div>
+          <div className="border-4 border-black">
+            <video
+              autoPlay
+              loop
+              muted
+              className="w-full h-auto max-h-[70vh] object-cover"
+              src="src/assects/pageEffect-vid-01.mp4"
+            />
+          </div>
+        </div>
+      </section>
+
+      <div className="h-[30vh]"></div>
+
+      {/* Section: Fourth */}
+      <section className="fourth min-h-screen w-full bg-gray-300 p-4 sticky top-0">
+        <div className="flex justify-between items-center">
+          <h1 className="text-[8vw] font-medium">Delivery At Dawn</h1>
+          <h2 className="text-[4vw]">View Project</h2>
+        </div>
+        <div className="border-2 border-dotted border-black p-4">
+          <div className="text-xs flex justify-between p-1">
+            <span>OVERSCAN</span>
+            <span>1577 x 974</span>
+          </div>
+          <div className="border-4 border-black">
+            <video
+              autoPlay
+              loop
+              muted
+              className="w-full h-auto max-h-[70vh] object-cover"
+              src="src/assects/pageEffect-vid-02.mp4"
+            />
+          </div>
+        </div>
+      </section>
+
+      <div className="h-[30vh]"></div>
+
+      {/* Section: Fifth */}
+      <section className="fifth min-h-screen w-full bg-gray-300 p-4 sticky top-0">
+        <div className="flex justify-between items-center">
+          <h1 className="text-[8vw] font-medium">Izzo</h1>
+          <h2 className="text-[4vw]">View Project</h2>
+        </div>
+        <div className="border-2 border-dotted border-black p-4">
+          <div className="text-xs flex justify-between p-1">
+            <span>OVERSCAN</span>
+            <span>1577 x 974</span>
+          </div>
+          <div className="border-4 border-black">
+            <video
+              autoPlay
+              loop
+              muted
+              className="w-full h-auto max-h-[70vh] object-cover"
+              src="src/assects/pageEffext-vid-03.mp4"
+            />
+          </div>
+        </div>
+      </section>
+
+      <div className="h-[40vh]"></div>
+
+      {/* Section: Sixth */}
+      <section className="sixth min-h-[160vh] w-full bg-stone-100 sticky top-0 p-4">
+        <h2 className="text-sm">/ MAKE YOUR MARK</h2>
+        <h1 className="text-[10vw] font-semibold leading-[10vh]">The Studio</h1>
+
+        <div className="h-[20vh]"></div>
+
+        <div className="flex flex-col lg:flex-row gap-8">
+          {/* Left Side: Images */}
+          <div className="relative w-full lg:w-1/2 p-4">
+            <img
+              className="w-full max-w-[90%] h-auto mx-auto rotate-[5deg]"
+              src="src/assects/images/Seaction-img-two.avif"
+              alt="Studio Image 2"
+            />
+            <img
+              className="absolute w-full max-w-[90%] h-auto top-24 left-0"
+              src="src/assects/images/Seacion-img-one.avif"
+              alt="Studio Image 1"
+            />
+          </div>
+
+          {/* Right Side: Text Content */}
+          <div className="w-full lg:w-1/2 p-4">
+            <h2 className="text-sm mb-2">/ THE LOWDOWN</h2>
+            <p className="text-base leading-relaxed">
+              We’re a globally renowned animation studio based in East LDN. We
+              work across advertising, interactive media, music videos and our
+              own original content.
+            </p>
+
+            <h2 className="text-sm mt-10 mb-2">/ AWARDS & MENTIONS</h2>
+            <div className="Awards grid grid-cols-2 gap-4 text-sm leading-relaxed">
+              <div className="space-y-1 underline underline-offset-4 decoration-1">
+                <p>BAFTA</p>
+                <p>AD YOUNG GUN AWARDS</p>
+                <p>MEOW ANIMATION AWARDS</p>
+                <p>CLIO</p>
+                <p>MEOW ARROWS</p>
+                <p>MEOW MUSIC VIDEO AWARDS</p>
+                <p>D&AD</p>
+              </div>
+              <div className="space-y-1">
+                <p>GQ</p>
+                <p>NME</p>
+                <p>THE VERGE</p>
+                <p>VARIETY</p>
+                <p>THE HOLLYWOOD REPORTER</p>
+                <p>THE LOS ANGELES TIMES</p>
+                <p>THE TONIGHT SHOW WITH JIMMY FALLON</p>
+                <p>BBC</p>
+                <p>WIRED MAGAZINE</p>
+                <p>THE INDEPENDENT</p>
+                <p>THE SUNDAY TIMES</p>
+                <p>ROLLING STONE</p>
+                <p>THE TELEGRAPH</p>
+                <p>PLAYBOY</p>
+                <p>COSMOPOLITAN</p>
+                <p>E ONLINE</p>
+                <p>NERDIST</p>
+                <p>NBC NEWS</p>
+                <p>POLYGON</p>
+                <p>HYPEBEAST</p>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h1 className="text-[6vw] font-bold leading-tight">
+                “BELLISSIMO”
+              </h1>
+              <h2 className="text-sm font-normal">– GUILLERMO DEL TORO</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Page2
